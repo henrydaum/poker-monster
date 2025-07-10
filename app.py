fb23b390690d56a9aeeace7d51513d8bc019d144
Henry Daum <henrydaum8609@gmail.com>
2025-07-09 20:17:39 -0400

    slight eror

--- app.py
+++ app.py
@@ -32,9 +32,9 @@ app.secret_key = 'a_very_secret_key'
 socketio = SocketIO(app)
 
 # Redis for memory
-redis_url = os.getenv('REDIS_URL')
+redis_url = os.getenv("redis://red-d1ng0bbipnbc73ak7j0g:6379")
 app.config['SESSION_TYPE'] = 'redis'
-app.config['SESSION_REDIS'] = redis.from_url("redis://red-d1ng0bbipnbc73ak7j0g:6379")
+app.config['SESSION_REDIS'] = redis.from_url(redis_url)
 
 # This uses Flask Session to run the cookie on the server side (the gamestate + hidden state is too large for browser)
 app.config["SESSION_PERMANENT"] = False
